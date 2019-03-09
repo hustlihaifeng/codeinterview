f39c4993ae3f43dfe5fce8987e56d4be69486a3c
hustlihaifeng <1049400300@qq.com>
2019-03-09 14:16:06 +0800

    add reverse and list_operations.md

---
+++ algorithm/list/list_operations.md
@@ -0,0 +1,12 @@
+
+# 目录
+- [翻转](#翻转)
+- [合并](#合并)
+
+# 翻转
+1. 关键点：将`newHead`初始化为nil，新链表从前往后涨。
+2. 代码见[reverse/main.go](reverse/main.go)
+
+# 合并
+1. 关键点：头结点为空会让代码变得复杂，我们可以新建一个`dummy`头结点，那么此后头结点都不为空，结果返回`dummy.Next`.
+2. 代码见[merge/main.go](merge/main.go)
---
+++ algorithm/list/reverse/main.go
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=206 lang=golang
+ *
+ * [206] Reverse Linked List
+ *
+ * https://leetcode.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (52.34%)
+ * Total Accepted:    522.8K
+ * Total Submissions: 988.4K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Reverse a singly linked list.
+ *
+ * Example:
+ *
+ *
+ * Input: 1->2->3->4->5->NULL
+ * Output: 5->4->3->2->1->NULL
+ *
+ *
+ * Follow up:
+ *
+ * A linked list can be reversed either iteratively or recursively. Could you
+ * implement both?
+ *
+ */
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var newHead *ListNode
+	pnode := head
+	for pnode != nil {
+		pnext := pnode.Next
+		pnode.Next = newHead
+		newHead = pnode
+		pnode = pnext
+	}
+	return newHead
+}
